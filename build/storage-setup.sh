0eff5c38215f2449aa8e6e832eca69c15c167d7e
fomalhautleo <fomalhautleo@163.com>
2022-03-19 13:43:08 +0800

    remove build directory

--- build/storage-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/home/jace/git/simple-distributed-storage-system/lib:$LD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=/home/jace/git/simple-distributed-storage-system/lib:$DYLD_LIBRARY_PATH
-export PATH=/home/jace/git/simple-distributed-storage-system/bin:$PATH
-export CMAKE_PREFIX_PATH=/home/jace/git/simple-distributed-storage-system/lib:$CMAKE_PREFIX_PATH
